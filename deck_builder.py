eb4791e4eef8f711b694e1d429d234eab1571016
Jason Curtis <curtisjj42@gmail.com>
2023-04-07 16:40:25 -0400

    initial structure suggestions for classes

---
+++ deck_builder.py
@@ -0,0 +1,21 @@
+class Deck:
+    def __init__(self):
+        self.suits = ["hearts", "clubs", "diamonds", "spades"]
+        self.face_cards = ["jack", "queen", "king", "ace"]
+
+    def build_deck(self):
+        for each in self.suits:
+            for i in range(1, 11):
+                card = Card(each, i)
+            for face in self.face_cards:
+                card = Card(each, face)
+
+    def deck_storage(self, card):
+        # linked list constructor
+        pass
+
+
+class Card:
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
