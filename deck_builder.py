d67681fafedbebd1038c80ca6084d6d17b055091
ZacharyArmandNEU <113147512+ZacharyArmandNEU@users.noreply.github.com>
2023-04-13 14:44:29 -0400

    Creates setter methods for Card class; adds self.next and self.prev for linked list implementation PLEASE feel free to revise if you want, I'm not married to any ideas

--- deck_builder.py
+++ deck_builder.py
@@ -1,28 +1,73 @@
 class Deck:
-    def __init__(self):
-        self.suits = ["hearts", "clubs", "diamonds", "spades"]
-        self.face_cards = ["jack", "queen", "king", "ace"]
+    def __init__(self,
+                 suits = ["hearts", "clubs", "diamonds", "spades"],
+                 face_cards = ["jack", "queen", "king", "ace"],
+                 top_spot = 10,
+                 jokers = False):
+        # make each suit names and face card names lowercase for later processing
+        self.suits = [each.lower() for each in suits]
+        self.face_cards = [each.lower() for each in face_cards]
+        # spot card is any non-face/non-ace but including ten
+        self.top_spot = top_spot
+        # using jokers?
+        self.jokers = jokers
+        # top and bottom of deck, occupied by a card
+        self.bottom = None
+        self.top = None
+        # list to hold all cards
+        self.cards = []
+
+        # create deck
+        self.build_deck()
 
     def build_deck(self):
+        # go through each suit
         for each in self.suits:
-            for i in range(1, 11):
+            # start with ace if needed - indicated in self.face_cards
+            if "ace" in self.face_cards:
+                card = Card(each, 1, "ace")
+                self.cards.append(card)
+            # go thru cards 2-10
+            for i in range(2, self.top_spot+1):
+                # create card
                 card = Card(each, i)
-            for face in self.face_cards:
-                card = Card(each, face)
+                # add card to list of cards
+                self.cards.append(card)
+            # create jack, queen, king if needed - check if face cards is not None
+            if self.face_cards is not None:
+                # loop thru each index in face_cards
+                for i in range(len(self.face_cards)):
+                    # skip if card is "ace"
+                    if self.face_cards[i] == "ace":
+                        pass
+                    # assign other face card values
+                    else:
+                        card = Card(each,
+                                    self.top_spot+1,
+                                    self.face_cards[i])
+                        self.cards.append(card)
 
-    def deck_storage(self, card):
-        # linked list constructor
-        pass
+    def deck_storage(self, cards):
+        for i in range(len(cards)):
+            # first card
+            if i == 0:
+                # make first card bottom of deck
+                self.bottom = cards[i]
+                cards.prev = None
+                cards.next = cards[i+1]
     
 
 
 class Card:
-    def __init__(self, suit=None, value=None):
+    def __init__(self, suit=None, value=None, name = None):
         self.suit = suit
         self.value = value
         # linked list elements
         self.next = None
         self.prev = None
+
+        # optional name for assigning face card values or joker values
+        self.card_name = name
 
     def __str__(self):
         """Returns first letter of suit and value"""
