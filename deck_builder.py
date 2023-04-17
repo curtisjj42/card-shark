96e8c7a155ad30ecb8e36f345fa1ceac16a7220b
Alex Kramer <80472342+Alex-Kramer0@users.noreply.github.com>
2023-04-17 14:38:50 -0400

    pull method added

--- deck_builder.py
+++ deck_builder.py
@@ -65,6 +65,47 @@ class Deck:
             if str(item) == card:
                 return index
             index += 1
+    
+    def pull(self, pull):
+        '''
+        Removes one card from the deck at a time,
+        :param pull: card that is to be pulled out of the deck, removed
+        :return:No returns, mutates self.cards list and individual card's pointers within Card class
+
+        Example for team to understand logic:
+            {0} -> {1} -> {2} -> {3}
+            if we remove {0}, we set: {1}.prev = {3} or index -1; {3}.next = {1} or index +1
+            if we remove {3}, we set: {0}.prev = {2} or index -1; {2}.next = {0} or 0
+            if we remove {1}, we set: {2}.prev = {0} or index -1; {0}.next = {2} or index +1
+        '''
+
+        index = self.find(pull) #finds index of the card you want to pull
+        if index == 0:
+            # If we pull / del the card at index 0
+            self.cards[index + 1].set_prev(len(self.cards)) # sets the card after to have .prev = last card in list
+            self.cards[len(self.cards) - 1].set_next(self.cards[index + 1]) # sets the card before to have .next = next card
+            self.cards.remove(self.cards[index]) # removes card
+
+        elif index == (len(self.cards) - 1):
+            # If we pull/ del the last card in the list
+            self.cards[0].set_prev(self.cards[index - 1]) # sets card[0] to .prev = last in list
+            self.cards[index - 1].set_next(self.cards[0]) # sets new last card to .next = first in list
+            self.cards.remove(self.cards[index]) # removes card
+
+        else:
+            # if we pull / del any card that is not in position 0 or in the last position in the list
+            self.cards[index + 1].set_prev(self.cards[index - 1]) # sets .prev
+            self.cards[index - 1].set_next(self.cards[index + 1]) # sets .next
+            self.cards.remove(self.cards[index]) # removes card
+
+    def pull_list(self, cards=list):
+        '''
+        Pulls / removes a list of cards from the deck
+        :parameter cards: a list of strings
+        :return: mutates list
+        '''
+        for card in cards:
+            self.pull(card.lower())
 
 class Card:
     def __init__(self, suit=None, name=None):
